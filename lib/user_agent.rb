9fc72c68efae9661f9aee65904dab01a2d865fbc
Jamie Macey <jamie@almlabs.com>
2008-11-19 15:29:56 -0500

    Identify some browsers and operating systems...
    Detected browsers:
    * Firefox
    * MSIE
    * Opera
    * Safari
    * Any other standards-following user agent
    
    Detected operating systems:
    * Linux
    * Mac OS X (multiple)
    * iPhone
    * Windows (multiple)
    * FreeBSD, NetBSD, OpenBSD
    * SunOS, Amiga, BeOS, IRIX, OS/2

--- lib/user_agent.rb
+++ lib/user_agent.rb
@@ -1,6 +1,164 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-module UserAgent
+class UserAgent
   VERSION = '0.0.1'
+  
+  attr_reader :browser_name, :browser_version
+  attr_reader :os_name, :os_version
+  
+  def initialize(user_agent)
+    @user_agent = user_agent
+    extract_products_from_agent_string
+    return if @products.empty?
+
+    identify_browser
+    identify_os
+  end
+  
+  def browser
+    [browser_name, browser_version].compact.join(' ')
+  end
+  
+  def os
+    [os_name, os_version].compact.join(' ')
+  end
+  
+private
+  def extract_products_from_agent_string
+    pattern = Regexp.new(
+      "([^/\s]*)" +                           # product token
+      "(/([^\s]*))?" +                        # optional version
+      "([\s]*\\[[a-zA-Z][a-zA-Z]\\])?" +      # optional old netscape
+      "[\s]*" +                               # eat space!
+      "(\\((([^()]|(\\([^()]*\\)))*)\\))?" +  # optional comment, allow one deep nested ()
+      "[\s]*"                                 # eat space!
+    )
+    @products = @user_agent.scan(pattern).map{|match|
+      [match[0], match[2], match[5]]
+    }.select{|product| !product[0].empty?}
+  end
+  
+  # Browser Identification
+  
+  def identify_browser
+    identify_browser_opera or
+    identify_browser_safari or
+    identify_browser_honest or
+    identify_browser_compatible or
+    identify_browser_mozilla or
+    identify_browser_other
+  end
+  
+  def identify_browser_opera
+    return unless @user_agent =~ /Opera/
+    
+    if opera = @products.detect{|product| product[0] == 'Opera'}
+      if opera[1].nil?
+        if @products[-2][0] == 'Opera'
+          @browser_version = @products[-1][0]
+        end
+      else
+        @browser_version = opera[1]
+      end
+      @browser_name = "Opera"
+    end
+  end
+  
+  def identify_browser_safari
+    return unless @user_agent =~ /Safari|iPhone/
+    
+    if version = @products.detect{|product| product[0] == 'Version'}
+      @browser_version = version[1]
+    elsif browser = @products.detect{|product| product[0] == 'Safari'}
+      @browser_version = browser[1]
+    end
+    @browser_name = 'Safari'
+  end
+  
+  def identify_browser_honest
+    honest_browsers = %w(Firefox Netscape Camino Mosaic Galeon)
+    if browser = @products.detect{|product| honest_browsers.include? product[0]}
+      @browser_version = browser[1]
+      @browser_name = browser[0]
+    end
+  end
+  
+  def identify_browser_compatible
+    compatible = /^compatible; ([^\s]+) ([^\s;]+)/
+    if browser = @products.detect{|product| product[0] == 'Mozilla' && product[2] =~ compatible}
+      # TODO? check_for_cloaked_products(AVANT_BROWSER, CRAZY_BROWSER);
+      @browser_version = $2
+      @browser_name = $1
+    end
+  end
+  
+  def identify_browser_mozilla
+    first = @products.first
+    if first[0] == 'Mozilla'
+      if first[1].to_f < 5.0
+        @browser_version = first[1]
+        @browser_name = 'Netscape'
+      else
+        first[2] =~ /rv:([^s]+)/
+        @browser_version = $1
+        @browser_name = first[0]
+      end
+    end
+  end
+  
+  def identify_browser_other
+    @browser_version = @products.first[1]
+    @browser_name = @products.first[0]
+  end
+  
+  # OS Identification
+  
+  def identify_os
+    @comment_elements = @products[0][2].split(/\s*;\s*/)
+    identify_os_windows or
+    identify_os_mac or
+    identify_os_linux or
+    identify_os_other
+  end
+  
+  def identify_os_windows
+    return unless element = @comment_elements.detect{|e| e =~ /^win.*\d/i}
+    @os_name = 'Windows'
+    @os_version = case element
+    when /98/: '98'
+    when /9x 4.90/: 'ME'
+    when /NT 4.0/: 'NT'
+    when /NT 5.0/: '2000'
+    when /NT 5.1/: 'XP'
+    when /NT 6.0/: 'Vista'
+    end
+  end
+  
+  def identify_os_mac
+    return unless element = @comment_elements.detect{|e| e =~ /Mac OS X/} or
+                  element = @comment_elements.detect{|e| e =~ /Macintosh/}
+    @os_name = case element
+    when /iphone/i
+      'iPhone'
+    else
+      'Mac OS X'
+    end
+    
+    if element =~ /(10_._.)/
+      @os_version = $1.gsub('_','.')
+    end
+  end
+  
+  def identify_os_linux
+    return unless element = @comment_elements.detect{|e| e =~ /linux/i}
+    @os_name = 'Linux'
+  end
+  
+  def identify_os_other
+    %w(FreeBSD NetBSD OpenBSD SunOS Amiga BeOS IRIX OS/2 Warp).each do |os|
+      os_regexp = Regexp.new(Regexp.escape(os))
+      if @comment_elements.detect{|e| e =~ os_regexp}
+        @os_name = os
+        return
+      end
+    end
+  end
 end
--- spec/user_agent_spec.rb
+++ spec/user_agent_spec.rb
@@ -1,11 +1,63 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
-describe "Place your specs here" do
-  
-  it "find this spec in spec directory" do
-    violated "Be sure to write your specs"
+describe UserAgent do
+
+  describe '#browser' do
+    it "should identify browser name and version (if available)" do
+      examples = {
+        "Firefox 0.9.3"     => "Mozilla/5.0 (Windows; U; Win98; de-DE; rv:1.7) Gecko/20040803 Firefox/0.9.3",
+        "Firefox 1.5.0.3"   => "Mozilla/5.0 (Windows; U; Win 9x 4.90; en-US; rv:1.8.0.3) Gecko/20060426 Firefox/1.5.0.3",
+        "Firefox 2.0.0.18"  => "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.8.1.18) Gecko/20081029 Firefox/2.0.0.18",
+        "Firefox 3.0b5"     => "Mozilla/5.0 (Windows; U; Windows NT 6.0; de; rv:1.9b5) Gecko/2008032620 Firefox/3.0b5",
+        "Firefox 3.1.6"     => "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.2) Gecko/2008092313 Ubuntu/8.04 (hardy) Firefox/3.1.6",
+        "MSIE 5.5"          => "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "MSIE 7.0"          => "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; WOW64; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.5.21022; .NET CLR 3.5.30729; .NET CLR 3.0.30618)",
+        "MSIE 8.0"          => "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; FunWebProducts; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506; InfoPath.1)",
+        "Opera 8.0"         => "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 8.0",
+        "Opera 9.02"        => "Opera/9.02 (Windows NT 5.1; U; pt-br)",
+        "Opera 9.50"        => "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 6.0; en) Opera 9.50",
+        "Opera 9.52"        => "Opera/9.52 (Windows NT 6.0; U; Opera/9.52 (X11; Linux x86_64; U); en)",
+        "Opera 9.61"        => "Mozilla/5.0 (Windows NT 5.1; U; en-GB; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.61",
+        "Opera 9.62"        => "Opera/9.62 (X11; Linux i686; U; en) Presto/2.1.1",
+        "Safari 3.1"        => "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_5_2; en-gb) AppleWebKit/526+ (KHTML, like Gecko) Version/3.1 iPhone",
+        "Safari 3.2"        => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; sv-se) AppleWebKit/525.26.2 (KHTML, like Gecko) Version/3.2 Safari/525.26.12",
+        "Safari"            => "Mozilla/5.0 (Macintosh; U; PPC Mac OS X; fr) AppleWebKit/412.7 (KHTML, like Gecko) Safari/412.5",
+        "Safari"            => "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.3) Gecko/2008092816 Mobile Safari 1.1.3",
+        "Safari"            => "Mozilla/5.0 (iPhone; U; CPU iPhone OS 2_1 like Mac OS X; fr-fr) AppleWebKit/525.18.1 (KHTML, like Gecko) Mobile/5F136",
+        "" => ""
+      }
+      examples.keys.sort.each do |expected|
+        agent = examples[expected]
+        UserAgent.new(agent).browser.should == expected
+      end
+    end
   end
   
+  describe '#os' do
+    it "should identify os name and version (if available)" do
+      examples = {
+        "Linux"           => "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.2) Gecko/2008092313 Ubuntu/8.04 (hardy) Firefox/3.1.6",
+        "Linux"           => "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.3) Gecko/2008092816 Mobile Safari 1.1.3",
+        "Mac OS X 10.5.2" => "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_5_2; en-gb) AppleWebKit/526+ (KHTML, like Gecko) Version/3.1 iPhone",
+        "Mac OS X 10.5.5" => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; sv-se) AppleWebKit/525.26.2 (KHTML, like Gecko) Version/3.2 Safari/525.26.12",
+        "Mac OS X"        => "Mozilla/5.0 (Macintosh; U; PPC Mac OS X; fr) AppleWebKit/412.7 (KHTML, like Gecko) Safari/412.5",
+        "OpenBSD"         => "Mozilla/5.0 (X11; U; OpenBSD i386; en-US; rv:1.8.1.7) Gecko/20070930 Firefox/2.0.0.7",
+        "Windows 2000"    => "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; en) Opera 8.0",
+        "Windows 98"      => "Mozilla/5.0 (Windows; U; Win98; de-DE; rv:1.7) Gecko/20040803 Firefox/0.9.3",
+        "Windows ME"      => "Mozilla/5.0 (Windows; U; Win 9x 4.90; en-US; rv:1.8.0.3) Gecko/20060426 Firefox/1.5.0.3",
+        "Windows Vista"   => "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; WOW64; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.5.21022; .NET CLR 3.5.30729; .NET CLR 3.0.30618)",
+        "Windows Vista"   => "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; FunWebProducts; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506; InfoPath.1)",
+        "Windows Vista"   => "Mozilla/5.0 (Windows; U; Windows NT 6.0; de; rv:1.9b5) Gecko/2008032620 Firefox/3.0b5",
+        "Windows XP"      => "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Windows XP"      => "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.8.1.18) Gecko/20081029 Firefox/2.0.0.18",
+        "iPhone"          => "Mozilla/5.0 (iPhone; U; CPU iPhone OS 2_1 like Mac OS X; fr-fr) AppleWebKit/525.18.1 (KHTML, like Gecko) Mobile/5F136",
+        "" => ""
+      }
+      examples.keys.sort.each do |expected|
+        agent = examples[expected]
+        UserAgent.new(agent).os.should == expected
+      end
+    end
+  end
+
 end
